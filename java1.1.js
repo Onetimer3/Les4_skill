9b301e707b879c94b8432577c480ce109fc6cdce
Onetimer3 <illiaoelle@gmail.com>
2024-10-14 11:49:19 +0200

    Add files via upload

---
+++ java1.1.js
@@ -0,0 +1,11 @@
+
+const date1 = new Date(1985, 6, 12); 
+const date2 = new Date(1955, 8, 23); 
+const date3 = new Date(2015, 7, 31);
+const date4 = new Date(1885, 0, 4);  
+
+
+console.log(`Datum 1: ${date1}`);
+console.log(`Datum 2: ${date2}`);
+console.log(`Datum 3: ${date3}`);
+console.log(`Datum 4: ${date4}`);
